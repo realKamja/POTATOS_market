b25116d7fb976481338eb62939076d7b14d39603
GamJaDo <kim0615157@gmail.com>
2023-08-03 01:55:56 +0900

    08/02 로그인, 회원가입 구현

---
+++ backend/temp/src/main/java/com/poteto/controller/MemberController.java
@@ -0,0 +1,70 @@
+package com.poteto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.poteto.dto.MemberDTO;
+import com.poteto.sevice.MemberService;
+
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+	
+	// 생성자 주입
+	private final MemberService memberService; // -> service클레스에 있는 자원을 사용할 권한을 줌
+	
+	@PostMapping("/member/login")
+	public String loginForm(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+		MemberDTO loginResult = memberService.login(memberDTO);
+		if(loginResult != null) {
+			// login 성공
+			session.setAttribute("loginEmail", loginResult.getMemberEmail());
+			return "main";
+		}
+		else {
+			// login 실패
+			return "login";
+		}
+	}
+	
+	// 회원가입 페이지 출력 요청
+	@GetMapping("/member/signin")
+	public String saveForm() {
+		return "signin";
+	}
+	
+	@PostMapping("/member/signin")
+	/*
+	public String save(@RequestParam("memberEmail") String memberEmail,
+						@RequestParam("memberPassword") String memberPassword,
+						@RequestParam("memberName") String memberName) {
+						*/
+	public String signin(@ModelAttribute MemberDTO memberDTO) {
+		memberService.signin(memberDTO);
+		
+		return "login";
+	}
+	
+	@GetMapping("/member/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "login";
+	}
+	
+	@PostMapping("/member/email-check")
+	// ajax를 사용 할 때에는 @ResponseBod를 사용한다.
+	public @ResponseBody String emailCheck(@RequestParam("memberEmail") String memberEmail) {
+		System.out.println("memberEmail = " + memberEmail);
+		
+		String checkResult = memberService.emailCheck(memberEmail);
+		return checkResult;
+	}
+}
---
+++ backend/temp/src/main/java/com/poteto/sevice/MemberService.java
@@ -0,0 +1,63 @@
+package com.poteto.sevice;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.poteto.dto.MemberDTO;
+import com.poteto.entity.MemberEntity;
+import com.poteto.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+	private final MemberRepository memberRepository;
+	
+	public MemberDTO login(MemberDTO memberDTO) {
+	
+		// 1. 회원이 입력한 이메일로 DB에서 조회를 함
+		// 2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치한지 판단
+		Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+			if(byMemberEmail.isPresent()) {
+				// 조회 결과가 있다(해당 이메일을 가진 회원 정보가 있다)
+				MemberEntity memberEntity = byMemberEmail.get(); // get을 통해 byMemberEntity에 씌워진 Optinal을 벗겨냄
+				if(memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+					// 비밀번호가 일치 
+					// entity -> dto 변환 후 리턴
+					MemberDTO dto = MemberDTO.toMemberDTO(memberEntity);
+					return dto;
+				}
+				else {
+					// 비밀번호 불일치(로그인 실패)
+					return null;
+				}
+		
+			}
+			else {
+				// 조회 결과가 없다(해당 이메일을 가진 회원이 없다)
+				return null;
+			}
+	}
+
+	public void signin(MemberDTO memberDTO) {
+		// 1. dto -> entity 변환
+		// 2. repository의 save 메서드 호출
+		MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+		memberRepository.save(memberEntity);
+		// repository의 save메서드 호출 (조건 entity객체를 넘겨줘야 함)
+	}
+	
+	public String emailCheck(String memberEmail) {
+		Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberEmail);
+		if(byMemberEmail.isPresent()) {
+			// 조회결과가 있다 -> 중복된 memberEmail이다
+			return null;
+		}
+		else {
+			// 조회결과가 있다 -> 사용할 수 있다
+			return "ok";
+		}	
+	}
+}
