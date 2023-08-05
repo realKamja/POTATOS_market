e76f6692bee1a65f14d66b0f7d66d5b4811b58bb
dandamdandam <glue0440@gmail.com>
2023-08-05 15:02:00 +0900

    Add mypage

---
+++ frontend/src/pages/mypage.js
@@ -0,0 +1,13 @@
+function Mypage(){
+    return(
+        <div className="mypage">
+            <button>설정</button>
+            <button>회원정보관리</button>
+
+            <button>판매 현황</button>
+            <button>구매 내역</button>
+        </div>
+    )
+}
+
+export default Mypage;
