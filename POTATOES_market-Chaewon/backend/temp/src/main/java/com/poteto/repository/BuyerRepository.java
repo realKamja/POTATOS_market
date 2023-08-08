0fcb71ef1b88d0f324ddd8fdfee050685b84c267
ChaewonYoon13 <128800307+ChaewonYoon13@users.noreply.github.com>
2023-08-08 10:13:11 +0900

    Delete POTATOES_market-Chaewon directory

--- POTATOES_market-Chaewon/backend/temp/src/main/java/com/poteto/repository/BuyerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.poteto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.poteto.entity.BuyerEntity;
-
-public interface BuyerRepository extends JpaRepository<BuyerEntity, Long>{
-	
-}
