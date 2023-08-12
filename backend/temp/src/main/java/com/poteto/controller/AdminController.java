05bbe4b30d4499768bb06b145f717c6ec12460da
ChaewonYoon13 <128800307+ChaewonYoon13@users.noreply.github.com>
2023-08-12 16:27:01 +0900

    1. Rest api 통신으로 변경 -사용하지 않는 html삭제 -자잘한 내용들이 많이 바뀌었으니 반드시 확인해주시기 바랍니다 2. 병수님 로그인 중복처리 병합 3. 시작페이지 /main or / 입력시 로그인 되지 않았으면 로그인창 주소 반한 4. 변경된 클래스: 모든 Controller

--- backend/temp/src/main/java/com/poteto/controller/AdminController.java
+++ backend/temp/src/main/java/com/poteto/controller/AdminController.java
@@ -4,15 +4,14 @@ import com.poteto.dto.MemberForAdminDTO;
 import com.poteto.sevice.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 @RequestMapping("/admin")
 public class AdminController {
 
@@ -23,22 +22,21 @@ public class AdminController {
         try {
             boolean isAdmin = memberService.isAdmin((String) session.getAttribute("loginId"));
             if (isAdmin == true) { // 로그인된 관리자 접근시
-                return "admin_main";
+                return "/admin/main"; //관리자 메인 페이지 주소 반환(아직 구현 안함)
             }
         } catch(Exception e){ // 로그인되지 않은 사용자 접근시
             e.getMessage();
-            return "no_access";
+            return "/main"; //(메인페이지 아직 구현 안함)
         }
-        return "no_access"; // 로그인된 일반 사용자 접근시
+        return "/main"; // 로그인된 일반 사용자 접근시
     }
 
     @GetMapping("/members")
-    @ResponseBody
     public List<MemberForAdminDTO> membersForAdmin(HttpSession session){
         try {
             boolean isAdmin = memberService.isAdmin((String) session.getAttribute("loginId"));
             if (isAdmin == true) { // 로그인된 관리자 접근시
-                return memberService.membersForAdmin();
+                return memberService.membersForAdmin(); // 회원 정보 반환
             }
         } catch(Exception e){ // 로그인되지 않은 사용자 접근시
             e.getMessage();
