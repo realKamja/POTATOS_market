336383bdecea2521cb7d4defde3ee0fc833713fb
dandamdandam <glue0440@gmail.com>
2023-08-16 21:15:07 +0900

    style sale form
    메인 가격 언디파인 확인바람

---
+++ temp/bin/main/static/js/main.js
@@ -0,0 +1,35 @@
+var fetchArticles=null;
+
+function scrollToShowcase() {
+    window.scroll({top: window.innerHeight, behavior: "smooth"});
+}
+
+$(document).ready(function() {
+    fetchArticles = () => {
+        $.ajax({
+            url: '/api/producter', // 게시글을 가져올 URL
+            type: 'GET',
+            dataType: 'json',
+            success: function(data) {
+                $('#post-container').empty();
+                
+                $.each(data, function(index, article) {
+                    var articleHTML = `
+                        <div class="post-item" title="상품 자세히 보기">
+                            <div class="imgs-container">
+                                <img src="/images/${article.producterImage}" alt="Image"> <br>
+                            </div>
+                            <h3>${article.producterTitle}</h2>
+                            <span>${article.ProducterPrice}</span>
+                        </div>
+                    `;
+                    $('#post-container').append(articleHTML);
+                });
+            },
+            error: function() {
+                console.log('오류 발생');
+            }
+        });
+    };
+    fetchArticles();
+});
